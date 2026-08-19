5ab23667d22b9322853ae5125f3e457d1d9fae5b
Ahmed Taha <mahmed8056@gmail.com>
2026-08-19 13:42:20 +0000

    Added operators script

---
+++ operators.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+a=10
+b=20
+echo "===================================="
+echo "ARITHMETIC OPERATORS IN PROGRAMMIONG"
+echo "===================================="
+
+echo "ADDITION+++++"
+c=$((a+b))
+echo "The total is $c"
+
+echo "==============="
+
+echo "SUBTRACTION-----"
+d=$((a-b))
+echo "The difference is $d"
+
+echo "===================="
+
+echo "MULTIPLICATION"
+m=$((a*b))
+echo "The product is $m"
+
+echo "===================="
+
+echo "DIVISION"
+div=$((b/a))
+echo "The division of a and b is $div"
+
+echo "====================="
+
+echo "MODLUS"
+mod=$((a%b))
+echo "The remainder of a and b is $mod"
+
+echo "====================="
+
+
+
