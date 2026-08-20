592cd31086e088935d688c79c2d46c524380a7cd
Ahmed Taha <mahmed8056@gmail.com>
2026-08-20 07:13:26 +0000

    Transferred Hardwareinfo.sh from main to scripts branch

--- Hardwareinfo.sh
+++ Hardwareinfo.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
 
-echo ""
-echo ""
 echo "------------"
 echo "Memory Space"
 echo "------------"
+echo ""
+echo ""
 free -h
 echo ""
-echo ""
-echo ""
-echo "Shows memory space in human readable format"
-
-echo ""
+echo "Show free space in human readable format"
 echo ""
 echo "----------"
 echo "Disk Usage"
 echo "----------"
 df -h
 echo ""
-echo ""
-echo ""
 echo "Shows disk space usage of mounted filesystems in human-readable format."
+echo ""
