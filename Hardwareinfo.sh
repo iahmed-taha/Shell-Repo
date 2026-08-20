5e85bd90b5d1b799e1c7da33e6ae00ccb041f744
Ahmed Taha <mahmed8056@gmail.com>
2026-08-20 06:46:39 +0000

    Added Script file which shows Hardware Information

---
+++ Hardwareinfo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo ""
+echo ""
+echo "------------"
+echo "Memory Space"
+echo "------------"
+free -h
+echo ""
+echo ""
+echo ""
+echo "Shows memory space in human readable format"
+
+echo ""
+echo ""
+echo "----------"
+echo "Disk Usage"
+echo "----------"
+df -h
+echo ""
+echo ""
+echo ""
+echo "Shows disk space usage of mounted filesystems in human-readable format."
